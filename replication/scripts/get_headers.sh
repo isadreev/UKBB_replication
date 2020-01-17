cef2342eace4d122e4b99ca21c326c264c151adf
ildar <i.sadreev@bristol.ac.uk>
2020-01-17 11:46:55 +0000

    headers update

---
+++ replication/scripts/get_headers.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+path_to_phesant="/mnt/storage/private/mrcieu/data/ukbiobank/phenotypic/applications/15825/2019-05-02/data/derived/phesant_mod"
+head -n 1 ${path_to_phesant}/*.txt | sed '/^$/d' > temp1
+paste <(grep "^==" temp1 | sed 's/==> //g' | sed 's/ <==//g') <(grep -v "^==" temp1) > temp2
+head temp2
