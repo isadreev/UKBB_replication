42aa522b89fcc68bdc13b3d9ac4a06ddd84582f8
ildar <i.sadreev@bristol.ac.uk>
2020-02-12 12:22:20 +0000

    Chr extraction

---
+++ replication/scripts/run_chr_extract.py
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+module add apps/qctool/2.0rc4
+
+qctool -bgen-bits 8 -g /mnt/storage/private/mrcieu/data/ukbiobank/genetic/variants/arrays/imputed/released/2018-09-18/data/dosage_bgen/data.chr01.bgen -incl-rsids snplist.txt -og extract.chr01.bgen
+qctool -bgen-bits 8 -g /mnt/storage/private/mrcieu/data/ukbiobank/genetic/variants/arrays/imputed/released/2018-09-18/data/dosage_bgen/data.chr02.bgen -incl-rsids snplist.txt -og extract.chr02.bgen
+qctool -bgen-bits 8 -g /mnt/storage/private/mrcieu/data/ukbiobank/genetic/variants/arrays/imputed/released/2018-09-18/data/dosage_bgen/data.chr03.bgen -incl-rsids snplist.txt -og extract.chr03.bgen
+qctool -bgen-bits 8 -g /mnt/storage/private/mrcieu/data/ukbiobank/genetic/variants/arrays/imputed/released/2018-09-18/data/dosage_bgen/data.chr04.bgen -incl-rsids snplist.txt -og extract.chr04.bgen
+qctool -bgen-bits 8 -g /mnt/storage/private/mrcieu/data/ukbiobank/genetic/variants/arrays/imputed/released/2018-09-18/data/dosage_bgen/data.chr05.bgen -incl-rsids snplist.txt -og extract.chr05.bgen
+qctool -bgen-bits 8 -g /mnt/storage/private/mrcieu/data/ukbiobank/genetic/variants/arrays/imputed/released/2018-09-18/data/dosage_bgen/data.chr06.bgen -incl-rsids snplist.txt -og extract.chr06.bgen
+qctool -bgen-bits 8 -g /mnt/storage/private/mrcieu/data/ukbiobank/genetic/variants/arrays/imputed/released/2018-09-18/data/dosage_bgen/data.chr07.bgen -incl-rsids snplist.txt -og extract.chr07.bgen
+qctool -bgen-bits 8 -g /mnt/storage/private/mrcieu/data/ukbiobank/genetic/variants/arrays/imputed/released/2018-09-18/data/dosage_bgen/data.chr08.bgen -incl-rsids snplist.txt -og extract.chr08.bgen
+qctool -bgen-bits 8 -g /mnt/storage/private/mrcieu/data/ukbiobank/genetic/variants/arrays/imputed/released/2018-09-18/data/dosage_bgen/data.chr09.bgen -incl-rsids snplist.txt -og extract.chr09.bgen
+qctool -bgen-bits 8 -g /mnt/storage/private/mrcieu/data/ukbiobank/genetic/variants/arrays/imputed/released/2018-09-18/data/dosage_bgen/data.chr10.bgen -incl-rsids snplist.txt -og extract.chr10.bgen
+qctool -bgen-bits 8 -g /mnt/storage/private/mrcieu/data/ukbiobank/genetic/variants/arrays/imputed/released/2018-09-18/data/dosage_bgen/data.chr11.bgen -incl-rsids snplist.txt -og extract.chr11.bgen
+qctool -bgen-bits 8 -g /mnt/storage/private/mrcieu/data/ukbiobank/genetic/variants/arrays/imputed/released/2018-09-18/data/dosage_bgen/data.chr12.bgen -incl-rsids snplist.txt -og extract.chr12.bgen
+qctool -bgen-bits 8 -g /mnt/storage/private/mrcieu/data/ukbiobank/genetic/variants/arrays/imputed/released/2018-09-18/data/dosage_bgen/data.chr13.bgen -incl-rsids snplist.txt -og extract.chr13.bgen
+qctool -bgen-bits 8 -g /mnt/storage/private/mrcieu/data/ukbiobank/genetic/variants/arrays/imputed/released/2018-09-18/data/dosage_bgen/data.chr14.bgen -incl-rsids snplist.txt -og extract.chr14.bgen
+qctool -bgen-bits 8 -g /mnt/storage/private/mrcieu/data/ukbiobank/genetic/variants/arrays/imputed/released/2018-09-18/data/dosage_bgen/data.chr15.bgen -incl-rsids snplist.txt -og extract.chr15.bgen
+qctool -bgen-bits 8 -g /mnt/storage/private/mrcieu/data/ukbiobank/genetic/variants/arrays/imputed/released/2018-09-18/data/dosage_bgen/data.chr16.bgen -incl-rsids snplist.txt -og extract.chr16.bgen
+qctool -bgen-bits 8 -g /mnt/storage/private/mrcieu/data/ukbiobank/genetic/variants/arrays/imputed/released/2018-09-18/data/dosage_bgen/data.chr17.bgen -incl-rsids snplist.txt -og extract.chr17.bgen
+qctool -bgen-bits 8 -g /mnt/storage/private/mrcieu/data/ukbiobank/genetic/variants/arrays/imputed/released/2018-09-18/data/dosage_bgen/data.chr18.bgen -incl-rsids snplist.txt -og extract.chr18.bgen
+qctool -bgen-bits 8 -g /mnt/storage/private/mrcieu/data/ukbiobank/genetic/variants/arrays/imputed/released/2018-09-18/data/dosage_bgen/data.chr19.bgen -incl-rsids snplist.txt -og extract.chr19.bgen
+qctool -bgen-bits 8 -g /mnt/storage/private/mrcieu/data/ukbiobank/genetic/variants/arrays/imputed/released/2018-09-18/data/dosage_bgen/data.chr20.bgen -incl-rsids snplist.txt -og extract.chr20.bgen
+qctool -bgen-bits 8 -g /mnt/storage/private/mrcieu/data/ukbiobank/genetic/variants/arrays/imputed/released/2018-09-18/data/dosage_bgen/data.chr21.bgen -incl-rsids snplist.txt -og extract.chr21.bgen
+qctool -bgen-bits 8 -g /mnt/storage/private/mrcieu/data/ukbiobank/genetic/variants/arrays/imputed/released/2018-09-18/data/dosage_bgen/data.chr22.bgen -incl-rsids snplist.txt -og extract.chr22.bgen
+
