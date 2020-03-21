397c1df8e4e09a56b391b494f26e6e7a6ca74698
Ildar Sadreev <i.sadreev@bristol.ac.uk>
2020-03-21 10:24:54 +0000

    path in rule create_dict

--- replication/scripts/phen_ID_mapping.r
+++ replication/scripts/phen_ID_mapping.r
@@ -4,8 +4,7 @@ a <- gwasinfo()
 b <- scan("ukb-b-idlist.txt", what="character")
 
 # Take only BMI and CHD
-#b <- c("ukb-b-19953",")
-b <- b[1:5]
+b <- c("ukb-b-19953","ukb-b-1668","ukb-b-3983","ukb-b-7436","ukb-b-16606")
 
 a <- subset(a, id %in% b)
 a$ukbbid <- sapply(strsplit(a$note, split=":"), function(x) x[1])
