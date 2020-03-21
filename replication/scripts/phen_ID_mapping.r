6e3c6a11cd1895c9e5a7c99a806fcdb3853f0b66
Ildar Sadreev <i.sadreev@bristol.ac.uk>
2020-03-21 18:28:48 +0000

    added 5 traits to Snakefile

--- replication/scripts/phen_ID_mapping.r
+++ replication/scripts/phen_ID_mapping.r
@@ -3,9 +3,6 @@ library(ieugwasr)
 a <- gwasinfo()
 b <- scan("ukb-b-idlist.txt", what="character")
 
-# Take only BMI and CHD
-#b <- c("ukb-b-19953","ukb-b-1668","ukb-b-3983","ukb-b-7436","ukb-b-16606")
-
 a <- subset(a, id %in% b)
 a$ukbbid <- sapply(strsplit(a$note, split=":"), function(x) x[1])
 
